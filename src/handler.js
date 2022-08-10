8297491ea1664cd4067760b09968547fde12e08a
unknown <leviettrung.social@gmail.com>
2022-08-11 03:54:26 +0700

    fix bug

--- src/handler.js
+++ src/handler.js
@@ -4,19 +4,19 @@ const header = {
   'Access-Control-Allow-Methods': 'GET',
   'Access-Control-Allow-Headers': 'Content-Type',
 }
-function splitMulti(str, tokens){
+function splitMulti(str, tokens) {
   var tempChar = tokens[0];
-  for(var i = 1; i < tokens.length; i++){
-      str = str.split(tokens[i]).join(tempChar);
+  for (var i = 1; i < tokens.length; i++) {
+    str = str.split(tokens[i]).join(tempChar);
   }
   str = str.split(tempChar);
   return str;
 }
 async function handleRequest(request) {
-  const lower_words = data.toLowerCase().split('\n')
+  const lower_words = data.toLowerCase().split('\n') //các từ trong data
   const params = new URL(request.url).searchParams;
   const content = params.get('content');
-  if(content === null){
+  if (content === null) {
     return new Response(JSON.stringify({
       error: 'No content provided',
     }), {
@@ -25,14 +25,19 @@ async function handleRequest(request) {
     });
   }
   const contents = splitMulti(content.toLowerCase(), [' ', '\n', ',', '.', '?', '!', ':', ';', '"', '\'']);
-  console.log(contents);
-  const filtered = contents.filter(function(n){ return n != '' });
+  const filtered = contents.filter(function (n) { return n != '' });
   let result = [];
   for (let i = 0; i < filtered.length; i++) {
-    if(!lower_words.includes(filtered[i]) && isNaN(filtered[i])){
-      result.push(filtered[i]);
+    if (!lower_words.includes(filtered[i]) && isNaN(filtered[i])) {
+      if (!lower_words.includes(filtered[i - 1] + " " + filtered[i])
+        && !lower_words.includes(filtered[i] + " " + filtered[i + 1])
+        && !lower_words.includes(filtered[i - 1] + " " + filtered[i] + " " + filtered[i + 1])) {
+        result.push(filtered[i]);
+      }
     }
   }
+  //unique result
+  result = result.filter((item, index) => result.indexOf(item) === index);
   const response = new Response(JSON.stringify({
     result: result,
   }), {
